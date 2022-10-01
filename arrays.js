13ba6f4f2d494f12eee54baf1b812f619648e457
Lauren Whitted <lauren.whitted13@gmail.com>
2022-09-30 22:47:26 -0500

    added arrays file

---
+++ arrays.js
@@ -0,0 +1,16 @@
+const digitalCraftsCrew = [
+    "Jamie",
+    "Dr. Vicki",
+    "Jada",
+    "Athena",
+    "Dee",
+    "Ernesto",
+    "Robbie"
+    ];
+
+//console.log(digitalCraftsCrew.length);
+digitalCraftsCrew.push("Lauren");
+//console.log(digitalCraftsCrew);
+digitalCraftsCrew.pop();
+//console.log(digitalCraftsCrew);
+console.log(digitalCraftsCrew.indexOf("Jamie"));
